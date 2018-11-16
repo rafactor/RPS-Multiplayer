40e36fbba8c1b5b24c2e1a73b56b0a8aa6590b14
rafactor <avila.rafael@gmail.com>
2018-11-16 17:45:52 -0500

    online presence recog

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,432 +1,224 @@
-// build front screen
 
-// select local ou online
+ 
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyCBeX6_5k8uBvj-A1kSroEMxJEQWJxBbmk",
+    authDomain: "rps-multiplayer-7196f.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-7196f.firebaseio.com",
+    projectId: "rps-multiplayer-7196f",
+    storageBucket: "rps-multiplayer-7196f.appspot.com",
+    messagingSenderId: "165306718406"
+  };
+
+  firebase.initializeApp(config);
 
-// login auth
-// select avatar
-// enter username
-// build play arena
-// show available players
-// invite a player
-// notification
-// acceptance
-// round selector (only creator)
-// select RPS
-// wait for opponent
-// run game js
-// store stats
-// display results
-// play again
-
-/* GLOBAL */
-const $body = $('body')
-// var title = 'RPS online'
-// const mode; //define online or local
-var loggedUser
-var player = [];
-var users = ['test'];
-var n = player.length
-var splashTimerID
-var avatar = ['fas fa-user-alt', 'fas fa-user-ninja', 'fas fa-user-tie',
-    'fas fa-user-md', 'fas fa-user-graduate', 'fas fa-user-secret',
-    'fas fa-user-injured', 'fas fa-user-astronaut'
-]
-
-// var db = firebase.database();
-
-
-
-var screen = {
-    initial() {
-        let $container = $('<div>');
-        $container.addClass('initial-container flex flex-column')
-
-        let $splash = $('<div>');
-        $splash.addClass('initial-wrapper-icons flex flex-row')
-
-        let $title = $('<h1>');
-        $title.addClass('title').html('i c o n i c')
-
-        icon.rock($splash)
-        icon.paper($splash)
-        icon.scissors($splash)
-
-        $container.append($title, $splash);
-        $body.append($container)
-    },
-
-    login(){
-        func.clearAll()
-        let $mainContainer = $('<div>');
-        $mainContainer.addClass('main-wrapper flex flex-column');
-
-        let $login = $('<button>');
-        $login.addClass('btn').attr({
-            'id':'login',
-        }).text('sign in')
-
-        let $newUser = $('<button>');
-        $newUser.addClass('btn').attr({
-            'id':'newUser',
-        }).text('new player');
-
-        $mainContainer.append($login, $newUser);
-        $body.append($mainContainer);
-    },
-
-    signin(){
-
-        func.clearAll()
-        let $mainContainer = $('<div>');
-        $mainContainer.addClass('main-wrapper flex flex-column');
-
-        let $placeholder = $('<div>')
-        $placeholder.addClass('placeholder')
-        .text('display sign in screen using Firebase')
-
-        let $placeholderButton = $('<div>')
-        $placeholderButton.addClass('btn')
-        .text('Sign in Test User').attr('id','signIn')
-
-        $mainContainer.append($placeholder, $placeholderButton)
-        $body.append($mainContainer);
-    },
-
-    player(){
-        let $container = $('.main-wrapper');
-        $container.empty();
-
-        icon.user($container);
-        let $navWrapper = $('<div>');
-        $navWrapper.addClass('navBar');
-
-        let $instruction = $('<p>');
-        $instruction.addClass('instructions')
-        .html('create your user name using only letters and numbers, without space and special characters:')
-
-        let $input = $('<input>');
-        $input.addClass('inputText').attr({
-            'id':'inputName',
-            'placeholder':'username'
-        })
-
-        icon.next($navWrapper)
-        $container.append($instruction, $navWrapper, $input)
-
-        let $error = $('<p>');
-        $error.addClass('errorMessage').html('');
-        $container.append($navWrapper, $error)
-    },
-    iconSelection(){
-        //clear the inverval timer used in the splash
-        clearInterval(splashTimerID)
-        $('body').empty();
-
-        let $mainContainer = $('<div>');
-        $mainContainer.addClass('main-wrapper flex flex-column');
-
-        let $instruction = $('<h3>');
-        $instruction.addClass('instruction').text('choose your avatar');
-
-        let $container = $('<div>');
-        $container.addClass('selectorWrapper');
-
-        avatar.forEach((e) => {
-            let $icon = $('<i>');
-            $icon.addClass(e + ' icon-selector')
-                .attr('id', e)
-            $container.append($icon)
-        });
-
-        // $($container).append($icon)
-        clearInterval(splashTimerID)
-        $($mainContainer).append($instruction, $container)
-        $('body').append($mainContainer)
-    },
-    opponent(){
-        console.log('opponent')
-        let $main = $('.main-wrapper')
-        $main.empty()
-
-        let $player = $('<div>')
-        $player.addClass('divPlayer center flex flex-row')
-
-        let $online = $('<div>')
-        $online.addClass('divOnline center')
-        
-        let $onlineLabel = $('<p>')
-        $onlineLabel.text('vs friend')
-
-        let $computer = $('<div>')
-        $computer.addClass('divComputer center')
-
-        let $computerLabel = $('<p>')
-        $computerLabel.text('vs computer')
-
-        let $opponent = $('<div>');
-        $opponent.addClass('divOpponent center flex flex-row')
-
-        
-        icon.friend($online)
-        icon.robot($computer)
-     
-        $online.append($onlineLabel)
-        $computer.append($computerLabel)
-        $opponent.append($online, $computer)
-        
-        $main.append( $opponent, $player)
-
-        screen.playerCard(n)
-    },
-    playerCard(n){
-        let $playerName = $('<h4>')
-        $playerName.attr('id','name').text(player[n].username)
-
-        let $playerWrapp = $('<div>');
-        $playerWrapp.addClass('playerAvatar')
-
-        let $scoreWrapp = $('<div>')
-        $scoreWrapp.addClass('playerScoreWrapp')
-
-        let $wins = $('<p>')
-        $wins.html('Wins: <span id="scoreWins">0</span>')
-        .attr('id','wins');
-
-        let $losses = $('<p>')
-        $losses.html('Losses: <span id="scoreLosses">0</span>')
-        .attr('id','losses');
-
-        let $points = $('<p>')
-        $points.html('Points: <span id="scorePoints">0</span>')
-        .attr('id','points');
-
-
-        $scoreWrapp.append($points, $wins, $losses)
-
-        icon.selected($playerWrapp)
-
-        $playerWrapp.append($playerName)
-
-        $('.divPlayer').append($playerWrapp, $scoreWrapp);
-        // $player.append($playerName);
-    },
-
-
-
-
-   
-    playerDetail(n) {
-        let $container = $('.main-wrapper flex flex-column');
-        $container.empty();
-
-        let $navWrapper = $('<div>');
-        $navWrapper.addClass('navBar');
-
-        let $instruction = $('<p>');
-        $instruction.html('create your user name')
-
-        $container.append($instruction)
-
-
-
-        screen.playerIcon(n, $container)
-
-        let $error = $('<p>');
-        $error.addClass('errorMessage').html('')
-
-        $container.append($navWrapper, $error)
-    },
-    playerIcon(n, place) {
-        let $icon = $('<i>')
-        $icon.addClass(player[n].icon).attr({
-            'id': 'selectedAvatar'
-        });
-
-        // let $instruction = $('<div>');
-        // $instruction.addClass('instructions')
-        // .text('enter your username:')
-
-        let $input = $("<input>")
-        $input.attr({
-            'id': 'input-name',
-            'type': 'text',
-            'placeholder': 'type your name'
-        })
-
-
-        place.append($icon, $input)
-    },
-
-    // backButton(place) {
-
-
-    //     let $leftButton = $('<i>')
-    //     $leftButton.addClass('fas fa-chevron-left')
-    //         .attr('id', 'backToAvatarSelection')
-    //     place.append($leftButton)
-
-    // },
   
-}
+  var db = firebase.database();
+  var connectionsRef = db.ref("/connections");
+  var connectedRef = db.ref(".info/connected");
+ 
+  //Initial Values
+  var email = "";
+  var password = "";
+  var userList = [];
+  var userKey = [];
+  var counter = 0;
+  var currentUser = {}
+  var conkey;
 
-var func = {
+  var app = {
+      signInForm() {
+        let $wrapper = $('#onlinePlayers');
+        $wrapper.empty()
 
-    initiate(){
-        clearInterval(splashTimerID);
-        $('body').empty();
+        let $newContainer = $('<div>');
+        $newContainer.addClass('form center').attr('id','sign-form')
+
+        let $form = $('<form>')
+        $form.addClass('container column')
+
+        let $emailLabel = $('<label>');
+        $emailLabel.attr('for','email').text('Email');
+
+        // let $passwordLabel = $('<label>');
+        // $passwordLabel.attr('for','password').text('Password');
+
+        // let $password = $('<input>');
+        // $password.attr({
+        //     'type':'password',
+        //     'placeholder':'Enter Password',
+        //     'name':'password'
+        // });
+
+        let $email = $('<input>');
+        $email.attr({
+            'type':'text',
+            'placeholder':'Enter Email',
+            'name':'email',
+            'id':'input-email'
+        });
+
+        let $login = $('<button>')
+        $login.attr({
+            'id':'btn-login',
+            'type':'submit'
+        }).text('Login')
+
+        let $remember = $('<label>')
+        $remember.html('<input type="checkbox" checked="checked" name="remember"> Remember me')
+            
+        let $cancel = $('<button>')
+        $cancel.attr({
+            'id':'cancelbtn',
+            'type':'button'
+        }).text('Cancel')
+
+        let $cancelWrapper = $('<div>')
+
+        // let $forgot = $('<span>')
+        // $forgot.addClass('forgot').html('Forgot <a href="#">password?</a>')
+
+        $cancelWrapper.append($cancel)
+        $form.append($emailLabel, $email, $login, $remember, $cancelWrapper);
+        $newContainer.append($form);
+        $wrapper.append($newContainer)
+      },
+      closeForm(){
+      $('#onlinePlayers').empty()
+
+      let $container = $('<div>');
+        $container.addClass('container center column').attr('id','sign-wrapper')
+
+        let $loginBtn = $('<button>');
+        $loginBtn.attr('id','signIn').text('Sign In');
+
+        // let $registerBtn = $('<button>');
+        // $registerBtn.attr('id','signUp').text('Register');
       
-        screen.login()
-        // console.log(db)
-        // if (db === null) {
-        //     console.log('create new player')
-        // } else {
-        //     console.log('welcome')
-        // }
-    },
-    saveName(newUser){
-        player[n] = new Player('Anonymous', n, 'anonymous' + n + '@anything.com');
-        users.push(newUser);
-        player[n].username = newUser;
+        $container.append($loginBtn);
 
-        $('.errorMessage').empty();
-        // icon.back($('.main-wrapper'), 'avatarSelection');
-        // icon.next($('.main-wrapper'),'selectOpponent');
-        // player[n].username = newUser;
-        screen.iconSelection();
-    },
+        $('#onlinePlayers').append($container)
+      },
+      signUpForm() {
+        let $wrapper = $('#onlinePlayers');
+        $wrapper.empty()
+        console.log('in')
 
+        let $newContainer = $('<div>');
+        $newContainer.addClass('form center').attr('id','sign-form')
+
+        let $form = $('<form>')
+        $form.addClass('container column')
+
+        let $emailLabel = $('<label>');
+        $emailLabel.attr('for','email').text('Email');
+
+        let $email = $('<input>');
+        $email.attr({
+            'id':'register-email',
+            'type':'text',
+            'placeholder':'Enter Email',
+            'name':'email'
+        });
+
+        let $passwordLabel = $('<label>');
+        $passwordLabel.attr('for','password').text('Password');
+
+        let $password = $('<input>');
+        $password.attr({
+            'type':'password',
+            'placeholder':'Enter Password',
+            'name':'password',
+            'id':'register-password'
+        });
+
+        let $passwordLabel2 = $('<label>');
+        $passwordLabel2.attr('for','password').text('Repeat Password');
+
+        let $password2 = $('<input>');
+        $password2.attr({
+            'type':'password',
+            'placeholder':'Repeat Password',
+            'name':'password'
+        });
+
+        
+
+        let $enter = $('<button>')
+        $enter.attr({
+            'id':'btnRegister',
+            'type':'button'
+        }).text('Register')
+
+          
+        let $cancel = $('<button>')
+        $cancel.attr({
+            'id':'cancelbtn',
+            'type':'button'
+        }).text('Cancel')
+
+        
+        let $signIn = $('<span>')
+        $signIn.addClass('signin').html('Already have an account? <a href="#">Sign In</a>')
     
-    checkName() {
-        var newUser = $(this).val().trim();
+        $form.append($emailLabel, $email, $passwordLabel, $password, $enter, $cancel);
+        $newContainer.append($form);
+        $wrapper.append($newContainer)
+      },
+      onlineView(){
+        $('#onlinePlayers').empty()  
+      },
+      findEmailID(email){
+          console.log('userId')
+        let n = userList.indexOf(key);
 
-        let numberOfUsers = users.length
-
-        if (numberOfUsers === 0) {
-            func.saveName(newUser);
-            console.log('zero');
+        if (n >= 0) {
+            currentUser = {
+                email: userList[existent],
+                key: userKey[existent]
+            }
+            console.log(currentUser)
+            return n
         } else {
-            if (users.indexOf(newUser) === -1) {
-                func.saveName(newUser);
-                console.log(users)
-                console.log('not found');
-            } else {
-                console.log('erro')
-                   $('.errorMessage').html('<small>This username already exists. <br><strong href="#">Login</strong> or create a new username.</small>')      
-               }
+            db.ref('users').push({
+                'email': email,
+
+            }) 
         }
-    },
-    chooseAvatar() {
-        player[n].icon = $(this).attr('id');
-        screen.opponent();
-        console.log('selected')
-    },
-    createPlayer() {
-        
-    },
-    clearAll() {
-        $('body').empty();
-    },
-    vsComputer(){
-        
+
+      }
+  }
+
+  //BTN LOGIN
+
+  // check if email exists
+
+
+  // ON LOAD
+// 1. get the list of registered emails
+// 2. get the list of online users
+
+  //Detecting Connection State
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+    // If they are connected..
+    if (snap.val()) {
+  
+      // Add user to the connections list.
+      var con = connectionsRef.push(true);
+  
+      // Remove user from the connection list when they disconnect.
+      con.onDisconnect().remove();
     }
-}
-var icon = {
-    selected(e) {
-        
-        let $image = $('<i>');
-        $image.addClass(player[n].icon).attr({
-            'id': 'user',
-        });
-        e.append($image)
-    },
-    user(e) {
-        let $image = $('<i>');
-        $image.addClass('fas fa-user-alt').attr({
-            'id': 'user',
-        });
-        e.append($image)
-    },
-    
-    rock(e) {
-        let $image = $('<i>');
-        $image.addClass('far fa-hand-rock initial-image').attr({
-            'id': 'rock',
-        });
-        e.append($image)
-    },
-
-    paper(e) {
-        let $image = $('<i>');
-        $image.addClass('far fa-hand-paper initial-image').attr({
-            'id': 'paper',
-        });
-        e.append($image)
-    },
-
-    scissors(e) {
-        let $image = $('<i>');
-        $image.addClass('far fa-hand-scissors initial-image').attr({
-            'id': 'scissors',
-        });
-        e.append($image)
-    },
-    next(e, id) {
-        let $image = $('<i>');
-        $image.addClass('fas fa-arrow-right').attr({
-            'id': id,
-            'data-gray': true
-        });
-        e.append($image)
-    },
-    back(e, id) {
-        let $image = $('<i>');
-        $image.addClass('fas fa-arrow-left').attr({
-            'id': id,
-        });
-        e.append($image)
-    },
-    friend(e) {
-        let $image = $('<i>');
-        $image.addClass('fas fa-user-friends').attr({
-            'id': 'friend'
-        });
-        e.append($image)
-    },
-    robot(e) {
-        let $image = $('<i>');
-        $image.addClass('fas fa-robot').attr({
-            'id': 'robot'
-        });
-        e.append($image)
-    }
-}
+  });
 
 
+//   $(document).on("click",".btn",btn.start)
+app.signInForm();
 
-function Player(first, last, email) {
-    this.firstName = first;
-    this.lastName = last;
-    this.username = 0;
-    this.email = email;
-    this.playerId = 'player' + n
-    this.icon = '';
-    this.wins = 0;
-    this.loses = 0
-    this.matches = 0;
-}
+  $(document).on('click','#signIn', app.signInForm)
+  $(document).on('click','#signUp', app.signUpForm)
+  $(document).on('click','#cancelbtn', app.closeForm)
 
-$(document).on("click", ".icon-selector", func.chooseAvatar)
-$(document).on('focusout', "#inputName", func.checkName)
-$(document).on('click', "#avatarSelection", screen.iconSelection)
-
-$(document).on('click',"#login", screen.signin);
-$(document).on('click',"#newUser", screen.player);
-
-$(document).on("click", ".divComputer", func.vsComputer)
-$(document).on("click", ".divOnline", func.multiplayer)
-
-
-
-
-screen.initial();
-splashTimerID = setInterval(func.initiate, 500)
